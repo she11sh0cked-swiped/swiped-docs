e8d1f4fc4eb0642341a003bd6df9050e97f919a5
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-25 14:50:23 +0200

    ➕ Add pandoc-gls

--- build-documentation.cmd
+++ build-documentation.cmd
@@ -4,6 +4,6 @@ mkdir build
 cd build
 
 echo ### Building documentation.pdf
-pandoc --fail-if-warnings --lua-filter ../pandoc/filters/date.lua --template ../pandoc/templates/pandoc-latex-template/eisvogel.tex --number-sections --listings --highlight-style pygments -o _documentation.pdf ../src/documentation.md
+pandoc --fail-if-warnings --lua-filter ../pandoc/filters/date.lua --lua-filter ../pandoc/filters/pandoc-gls/pandoc-gls.lua --template ../pandoc/templates/pandoc-latex-template/eisvogel.tex --number-sections --listings --highlight-style pygments -o _documentation.pdf ../src/documentation.md
 pdftk _documentation.pdf ../src/appendix/application.pdf cat output documentation.pdf
 del /F _documentation.pdf
