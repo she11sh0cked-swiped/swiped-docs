d2cc3a67f0381a9299d437e1ceb4c4e45fac1a73
she11sh0cked <22623152+she11sh0cked@users.noreply.github.com>
2021-04-30 20:09:40 +0200

    📝 Add ihk-annex

--- build-documentation.cmd
+++ build-documentation.cmd
@@ -5,5 +5,5 @@ cd build
 
 echo ### Building documentation.pdf
 pandoc --fail-if-warnings --lua-filter ../pandoc/filters/date.lua --lua-filter ../pandoc/filters/pandoc-gls/pandoc-gls.lua -F mermaid-filter.cmd --template ../pandoc/templates/pandoc-latex-template/eisvogel.tex --number-sections --listings --pdf-engine=lualatex -V monofont="Fira Code" -V lang=de -o _documentation.pdf ../src/documentation.md
-pdftk _documentation.pdf ../src/appendix/application.pdf cat output documentation.pdf
+pdftk _documentation.pdf ../src/appendix/ihk-annex.pdf ../src/appendix/application.pdf cat output documentation.pdf
 del /F _documentation.pdf
